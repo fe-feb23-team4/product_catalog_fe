dee47f5c5c638cce9cf24882ec15d0e4964585e2
Vyacheslav <haunt.beta@gmail.com>
2023-06-09 10:18:01 +0300

    second attempt to push

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,20 @@ const Header = () => {
   const [isActive] = useState(false);
 
   return (
-    <header className={`${styles.header} ${isActive ? styles['header--active'] : ''}`}>
+    <header
+      className={`${styles.header} ${isActive ? styles['header--active'] : ''}`}
+    >
       <nav className={styles.header__nav}>
         <ul className={styles.header__navList}>
           <li className={styles.header__navItem}>
-            <a href="/" className={styles.header__navLink}>Home</a>
+            <a href="/" className={styles.header__navLink}>
+              Home
+            </a>
           </li>
           <li className={styles.header__navItem}>
-            <a href="/about" className={styles.header__navLink}>About</a>
+            <a href="/about" className={styles.header__navLink}>
+              About
+            </a>
           </li>
           {/* Додайте інші пункти меню */}
         </ul>

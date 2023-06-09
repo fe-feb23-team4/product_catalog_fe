2c5ffdc736dfa2b58b8a9d06675e3fc68c3ddae1
Vyacheslav <haunt.beta@gmail.com>
2023-06-09 11:08:11 +0300

    fix media queries in mixins, add reset.scss file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Header.module.scss';
+import { Nav } from '../Nav/Nav';
 
 const Header = () => {
-  const [isActive] = useState(false);
-
   return (
-    <header
-      className={`${styles.header} ${isActive ? styles['header--active'] : ''}`}
-    >
-      <nav className={styles.header__nav}>
-        <ul className={styles.header__navList}>
-          <li className={styles.header__navItem}>
-            <a href="/" className={styles.header__navLink}>
-              Home
-            </a>
-          </li>
-          <li className={styles.header__navItem}>
-            <a href="/about" className={styles.header__navLink}>
-              About
-            </a>
-          </li>
-          {/* Додайте інші пункти меню */}
-        </ul>
-      </nav>
+    <header className={styles.header}>
+      <Nav />
     </header>
   );
 };
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,20 @@
+export const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/phones">Phones</a>
+        </li>
+        <li>
+          <a href="/tablets">Tablets</a>
+        </li>
+        <li>
+          <a href="/accesories">Accesories</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};

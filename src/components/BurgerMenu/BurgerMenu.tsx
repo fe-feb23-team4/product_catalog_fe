b735c7fb98053cdff28564a6d2103bcc3f5e095d
Vyacheslav <haunt.beta@gmail.com>
2023-06-13 09:27:21 +0300

    test commit

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import cn from 'classnames';
-import { HashRouter, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import cl from './BurgerMenu.module.scss';
 import logo from '../../assets/Logo.svg';
 import close from '../../assets/Close.svg';
@@ -23,80 +23,82 @@ export const BurgerMenu: FC<Props> = ({
   };
 
   return (
-    <HashRouter>
-      <div
-        className={menuClass}
-        role="button"
-        tabIndex={0}
-        onKeyDown={handleClick}
-        onClick={(e) => {
-          if ((e.target as HTMLElement).tagName === 'A') {
-            handleClick();
-          }
-        }}
-      >
-        <header className={cl.header}>
-          <NavLink className={cl.logo} to="/">
-            <img src={logo} alt="logo_icon" />
-          </NavLink>
+    <div
+      className={menuClass}
+      role="button"
+      tabIndex={0}
+      onKeyDown={handleClick}
+      onClick={(e) => {
+        if ((e.target as HTMLElement).tagName === 'A') {
+          setIsBurgerActive(!isBurgerActive);
+        }
+      }}
+    >
+      <header className={cl.header}>
+        <NavLink
+          className={cl.logo}
+          to="/"
+          onClick={() => setIsBurgerActive(!isBurgerActive)}
+        >
+          <img src={logo} alt="logo_icon" />
+        </NavLink>
 
-          <button
-            type="button"
-            className={cl.close_button}
-            onClick={handleClick}
-          >
-            <img src={close} alt="close_icon" />
-          </button>
-        </header>
+        <button
+          type="button"
+          className={cl.close_button}
+          onClick={() => setIsBurgerActive(!isBurgerActive)}
+        >
+          <img src={close} alt="close_icon" />
+        </button>
+      </header>
 
-        <nav className={cl.nav}>
-          <ul className={cl.ul}>
-            <li className={cl.li}>
-              <NavLink
-                to="/"
-                className={({ isActive }) => cn(cl.a, {
-                  [cl.active]: isActive,
-                })}
-              >
-                Home
-              </NavLink>
-            </li>
+      <nav className={cl.nav}>
+        <ul className={cl.ul}>
+          <li className={cl.li}>
+            <NavLink
+              to="/"
+              className={({ isActive }) => cn(cl.a, {
+                [cl.active]: isActive,
+              })}
+            >
+              Home
+            </NavLink>
+          </li>
 
-            <li className={cl.li}>
-              <NavLink
-                to="/phones"
-                className={({ isActive }) => cn(cl.a, {
-                  [cl.active]: isActive,
-                })}
-              >
-                Phones
-              </NavLink>
-            </li>
+          <li className={cl.li}>
+            <NavLink
+              to="/phones"
+              className={({ isActive }) => cn(cl.a, {
+                [cl.active]: isActive,
+              })}
+            >
+              Phones
+            </NavLink>
+          </li>
 
-            <li className={cl.li}>
-              <NavLink
-                to="/tablets"
-                className={({ isActive }) => cn(cl.a, {
-                  [cl.active]: isActive,
-                })}
-              >
-                Tablets
-              </NavLink>
-            </li>
+          <li className={cl.li}>
+            <NavLink
+              to="/tablets"
+              className={({ isActive }) => cn(cl.a, {
+                [cl.active]: isActive,
+              })}
+            >
+              Tablets
+            </NavLink>
+          </li>
 
-            <li className={cl.li}>
-              <NavLink
-                to="/accessories"
-                className={({ isActive }) => cn(cl.a, {
-                  [cl.active]: isActive,
-                })}
-              >
-                Accessories
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </HashRouter>
+          <li className={cl.li}>
+            <NavLink
+              to="/accessories"
+              className={({ isActive }) => cn(cl.a, {
+                [cl.active]: isActive,
+              })}
+            >
+              Accessories
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
---
+++ src/components/Nav/AddButtons/AddButtons.tsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
+import cl from './AddButtons.module.scss';
+
+import heart from '../../../assets/Heart.svg';
+import cart from '../../../assets/Cart.svg';
+
+export const AddButtons = () => {
+  return (
+    <div className={cl.add_buttons}>
+      <NavLink
+        to="/favourite"
+        className={({ isActive }) => cn(
+          cl.button, { [cl.active]: isActive },
+        )}
+      >
+        <img src={heart} alt="heart_icon" />
+      </NavLink>
+
+      <NavLink
+        to="/cart"
+        className={({ isActive }) => cn(
+          cl.button, { [cl.active]: isActive },
+        )}
+      >
+        <img src={cart} alt="cart_icon" />
+      </NavLink>
+    </div>
+  );
+};

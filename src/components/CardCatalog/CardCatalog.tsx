2e024e478a9fb2581d2fb2ede51b3a8a81b1b6aa
Yaroslav <kbshnik2020@gmail.com>
2023-06-13 11:20:57 +0300

    added grid template

--- src/components/CardCatalog/CardCatalog.tsx
+++ src/components/CardCatalog/CardCatalog.tsx
@@ -4,15 +4,20 @@ import cl from './CardCatalog.module.scss';
 export const CardCatalog = () => {
   return (
     <div className={cl.product_catalog}>
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-
+      <div className={cl.grid}>
+        <div className={cl.grid__item__1_6}>
+          <CardItem />
+        </div>
+        <div className={cl.grid__item__7_12}>
+          <CardItem />
+        </div>
+        <div className={cl.grid__item__13_18}>
+          <CardItem />
+        </div>
+        <div className={cl.grid__item__19_24}>
+          <CardItem />
+        </div>
+      </div>
     </div>
   );
 };

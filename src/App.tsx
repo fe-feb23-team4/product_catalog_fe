acb1fd39bac52e0b96739f4ea75c51f7f9bd4d3c
Yaroslav Pelenychka <125311355+yapelenua@users.noreply.github.com>
2023-06-13 11:31:11 +0300

    Page grid and adaptive layout (#20)
    * created grid layout
    
    * added grid template
    
    * deleted useless files

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.scss';
 import { CardItem } from './components/CardItem';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
+import { CardCatalog } from './components/CardCatalog/CardCatalog';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <CardItem />
+      <CardCatalog />
       <Footer />
     </div>
   );

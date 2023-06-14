3c30c80dc9f2d16a69071dfff523088115191b1c
Rage1502 <karaulchenkoilja@gmail.com>
2023-06-14 19:16:55 +0300

    hot-fix

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -4,9 +4,12 @@ export const HomePage = () => {
   return (
     <div className={cl.container}>
       <div className={cl.grid}>
-        <h1 className={`${cl.title} ${cl.grid__item} ${cl.grid__item__desktop_1_16} ${cl.grid__item__tablet_8_12}`}>Welcome to Nice Gadgets store!</h1>
+        <h1
+          className={`${cl.title} ${cl.grid__item} ${cl.grid__item__desktop_1_16} ${cl.grid__item__tablet_8_12}`}
+        >
+          Welcome to Nice Gadgets store!
+        </h1>
       </div>
-
     </div>
   );
 };

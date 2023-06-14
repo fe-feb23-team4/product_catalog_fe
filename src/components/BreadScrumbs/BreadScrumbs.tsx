9476c63b1e20ec6498ab70ab757c37a0862c6c65
Yaroslav Pelenychka <125311355+yapelenua@users.noreply.github.com>
2023-06-14 13:24:50 +0300

    created breadscrums component (#25)

---
+++ src/components/BreadScrumbs/BreadScrumbs.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HomeIcon } from './HomeIcon';
+import { Arrow } from './arrow';
+import cl from './BreadScrumbs.module.scss';
+
+interface Props {
+  category: 'Phones' | 'Tablets' | 'Accessories' | 'Favorites';
+  currentPage?: string;
+}
+
+export const Breadscrumbs: FC<Props> = ({ category, currentPage }) => {
+  return (
+    <div className={cl.breadcrumbs}>
+      <NavLink to="/">
+        <HomeIcon />
+      </NavLink>
+
+      <Arrow />
+
+      {currentPage ? (
+        <>
+          <NavLink to={`#/${category}`} className={cl.breadcrumbs__link}>
+            {category}
+          </NavLink>
+
+          <Arrow />
+
+          <span className={cl.breadcrumbs__text}>
+            {currentPage}
+          </span>
+        </>
+      ) : (
+        <span className={cl.breadcrumbs__text}>
+          {category}
+        </span>
+      )}
+    </div>
+  );
+};
+
+Breadscrumbs.defaultProps = {
+  currentPage: '',
+};
--- src/components/FavoritePage/FavoritePage.tsx
+++ src/components/FavoritePage/FavoritePage.tsx
@@ -1,8 +1,12 @@
 import cl from './FavoritePage.module.scss';
+import { Breadscrumbs } from '../BreadScrumbs/BreadScrumbs';
 
 export const FavoritePage = () => {
+  const category = 'Favorites';
+
   return (
     <div className={cl.container}>
+      <Breadscrumbs category={category} />
       <h1 className={cl.title}>Favorites</h1>
     </div>
   );

9bb5041283ef202072ca1d5b078476c3a6397d6b
Aleksandr <alexnovaklawyer@gmail.com>
2023-06-14 11:47:18 +0300

    template for base pages added

---
+++ src/components/TabletsPage/TabletsPage.tsx
@@ -0,0 +1,9 @@
+import cl from './TabletsPage.module.scss';
+
+export const TabletsPage = () => {
+  return (
+    <div className={cl.container}>
+      <h1 className={cl.title}>Tablets</h1>
+    </div>
+  );
+};

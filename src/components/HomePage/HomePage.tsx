2e2798919537ec718e690006196113a4e84511a1
Rage1502 <karaulchenkoilja@gmail.com>
2023-06-14 18:55:49 +0300

    add container and fix style

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
-import { CardItem } from '../CardItem';
 import cl from './HomePage.module.scss';
 
 export const HomePage = () => {
   return (
     <div className={cl.container}>
-      <h1 className={cl.title}>Home page</h1>
+      <div className={cl.grid}>
+        <h1 className={`${cl.title} ${cl.grid__item} ${cl.grid__item__desktop_1_16} ${cl.grid__item__tablet_8_12}`}>Welcome to Nice Gadgets store!</h1>
+      </div>
 
-      <CardItem />
     </div>
   );
 };

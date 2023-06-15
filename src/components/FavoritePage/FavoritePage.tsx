26f9720f2f497cd55dffb85c090d83c32cf4a99f
Yaroslav Pelenychka <125311355+yapelenua@users.noreply.github.com>
2023-06-16 00:51:24 +0300

    Working with localhost in favorite page (#29)
    * added count button
    
    * added count button and fixed localstorage
    
    * test (#28)
    
    * test
    
    * add basic sliderItem
    
    * added count button
    
    * deletet console log
    
    * added count button and fixed localstorage
    
    * deleted coments
    
    ---------
    
    Co-authored-by: Rage1502 <57044635+Rage1502@users.noreply.github.com>

--- src/components/FavoritePage/FavoritePage.tsx
+++ src/components/FavoritePage/FavoritePage.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react';
 import cl from './FavoritePage.module.scss';
 import { Breadscrumbs } from '../BreadScrumbs/BreadScrumbs';
 
 export const FavoritePage = () => {
   const category = 'Favorites';
+
+  const [favoriteItems, setFavoriteItems] = useState([]);
+
+  useEffect(() => {
+    const getFavData = () => {
+      const storedItems = localStorage.getItem('AddedToFavorite');
+
+      if (storedItems) {
+        const parsedItems = JSON.parse(storedItems);
+
+        setFavoriteItems(parsedItems);
+      }
+    };
+
+    const intervalId = setInterval(getFavData);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [favoriteItems]);
 
   return (
     <div className={cl.container}>

09f354b01474db2782f8fa138cfb51378509149a
Rage1502 <57044635+Rage1502@users.noreply.github.com>
2023-06-19 13:46:21 +0300

    Add slider Basic + Navigate (#41)
    * test
    
    * add basic banner
    
    * add basic slider
    
    * add basic slider + route
    
    ---------
    
    Co-authored-by: Oleksandr Novak <113321536+Kavon-Skela@users.noreply.github.com>

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,45 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+import './Slider.scss';
+import {
+  A11y, EffectFade, Navigation, Pagination, Scrollbar,
+} from 'swiper';
+
+export const Slider = () => {
+  return (
+    <div className="main-banner">
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
+        effect="fade"
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        className="swiper-container"
+      >
+        <SwiperSlide>
+          <img
+            src="/product_catalog_fe/carousel/carousel_1.png"
+            alt="carousel"
+            className="carousel-img"
+          />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src="/product_catalog_fe/carousel/carousel_007.png"
+            alt="carousel"
+            className="carousel-img"
+          />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src="/product_catalog_fe/carousel/carousel_008.png"
+            alt="carousel"
+            className="carousel-img"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};

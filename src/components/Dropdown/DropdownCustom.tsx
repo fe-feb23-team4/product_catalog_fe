f734bc35a3ef09b5ca1f9c82ad7be57adb6d85dc
Aleksandr <alexnovaklawyer@gmail.com>
2023-06-20 16:22:16 +0300

    bugs fixed

--- src/components/Dropdown/DropdownCustom.tsx
+++ src/components/Dropdown/DropdownCustom.tsx
@@ -1,4 +1,4 @@
-import { FC, MouseEvent } from 'react';
+import React, { FC } from 'react';
 import cn from 'classnames';
 import cl from './Dropdown.module.scss';
 import arrowUp from '../../assets/ArrowUp.svg';
@@ -36,27 +36,29 @@ export const DropdownCustom: FC<Props> = ({
           {currentOption}
           <img src={isActive ? arrowDown : arrowUp} alt="selection-arrow" />
         </button>
-        <ul
-          className={cn(cl.list, {
-            [cl.hidden]: !isActive,
-          })}
-        >
-          {options.map((option) => (
-            <li className={cl.list_item} key={option}>
-              <a
-                className={cl.link}
-                href="/"
-                onClick={(event) => {
-                  event.preventDefault();
-                  handleClick(!isActive);
-                  handleSelect(event);
-                }}
-              >
-                {option}
-              </a>
-            </li>
-          ))}
-        </ul>
+        {isActive && (
+          <ul
+            className={cn(cl.list, {
+              [cl.hidden]: !isActive,
+            })}
+          >
+            {options.map((option) => (
+              <li className={cl.list_item} key={option}>
+                <a
+                  className={cl.link}
+                  href="/"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    handleClick(!isActive);
+                    handleSelect(event);
+                  }}
+                >
+                  {option}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
